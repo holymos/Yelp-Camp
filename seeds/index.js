834fef6e71ccd364284043c35e676aac9f6927ea
unknown <moises.pacifico@hotmail.com>
2021-01-20 23:51:58 -0300

    Add Controllers and Star Ratings

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDb = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const randomPrice = Math.floor(Math.random() * 30) + 5;
         const camp = new Campground ({
-            author: '6008567d0951396089221326',
+            author: '6008d0f27ebaf141fcee5a45',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
